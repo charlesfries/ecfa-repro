302e48b6ec66c8c1536f50faf7ff1d2a0c6b5807
Charles Fries <charliefries@icloud.com>
2021-12-10 12:37:40 -0800

    Data fetching

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,6 +2,23 @@ import Route from '@ember/routing/route';
 
 export default class ApplicationRoute extends Route {
   model() {
-    return this.store.findAll('post');
+    // this._makeFakeData();
+    return this.store.findRecord('post', 'tC0XF90NS9jNR6nIcJHh'); // update me after making fake data
+  }
+
+  async _makeFakeData() {
+    const post = this.store.createRecord('post', {
+      title: 'The Title',
+      body: 'The Body',
+    });
+    await post.save();
+
+    for (let i = 0; i< 10; i++) {
+      const comment = this.store.createRecord('comment', {
+        post,
+        body: 'Here is a comment.',
+      });
+      await comment.save();
+    }
   }
 }
